50b05b30a2717fde026a8d1b00a30d66264b9a4d
BlockAyusTred <109907283+BlockAyusTred@users.noreply.github.com>
2022-12-18 02:00:13 +0530

    Add files via upload

---
+++ Lottery/Smart Contract/Lottery.sol
@@ -0,0 +1,137 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract Lottery is VRFConsumerBaseV2 {
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256 randomWords;
+    }
+    mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    // Your subscription ID.
+    uint64 s_subscriptionId;
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    bytes32 keyHash =
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+
+    address public owner;
+    address payable[] public players;
+    uint public lotteryId;
+    mapping(uint => address payable) public lotteryHistory;
+
+    uint256 private randomResult;
+    bool public fullfilled;
+
+// the address that you see in VRFConsumerBaseV2 parameter is Goerli vrf coordinator address ( link - https://docs.chain.link/vrf/v2/subscription/supported-networks#goerli-testnet)
+    constructor(uint64 subscriptionId)
+        VRFConsumerBaseV2(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D)
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(
+            0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D
+        );
+        s_subscriptionId = subscriptionId;
+
+        owner = msg.sender;
+        lotteryId = 1;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function requestRandomWords()
+        private
+        onlyOwner
+        returns (uint256 requestId)
+    {
+        // Will revert if subscription is not set and funded.
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: 0,
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords[0];
+    }
+
+    function getRequestStatus(uint256 _requestId)
+        private
+        view
+        returns (bool fulfilled, uint256 randomWords)
+    {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+
+    function getWinnerByLottery(uint lottery)
+        public
+        view
+        returns (address payable)
+    {
+        return lotteryHistory[lottery];
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        // address of player entering lottery
+        players.push(payable(msg.sender));
+    }
+
+    function pickWinner() public onlyOwner {
+        uint256 rqID = requestRandomWords();
+        randomResult = uint256(s_requests[rqID].randomWords);
+    }
+
+    function payWinner() public {
+        uint index = randomResult % players.length;
+        players[index].transfer(address(this).balance);
+
+        lotteryHistory[lotteryId] = players[index];
+        lotteryId++;
+
+        // reset the state of the contract
+        players = new address payable[](0);
+    }
+}
